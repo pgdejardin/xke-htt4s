0b9f8493ca20a3b1991cc22bd86e9fc193236e5c
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2019-03-31 17:28:46 +0200

    feat(liveCoding): connect to server

--- src/main/scala/fr/xebia/http4s/Server.scala
+++ src/main/scala/fr/xebia/http4s/Server.scala
@@ -3,10 +3,10 @@ package fr.xebia.http4s
 import cats.effect._
 import cats.implicits._
 import doobie.util.ExecutionContexts
-import fr.xebia.http4s.domain.{BookService, BookValidation}
+import fr.xebia.http4s.domain.{BookService, BookValidation, DiscountFromCart}
 import fr.xebia.http4s.infrastructure.config.{DatabaseConfig, LibraryConfig}
-import fr.xebia.http4s.endpoint.BookEndpoints
-import fr.xebia.http4s.infrastructure.repository.doobiedb.DoobieBookRepository
+import fr.xebia.http4s.endpoint.{BookEndpoints, CartEndpoints}
+import fr.xebia.http4s.infrastructure.repository.doobiedb.{DoobieBookRepository, DoobieCartRepository}
 import io.circe.config.parser
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
@@ -20,11 +20,13 @@ object Server extends IOApp {
       txnEc  <- ExecutionContexts.cachedThreadPool[F]
       xa     <- DatabaseConfig.dbTransactor(conf.db, connEc, txnEc)
       //      bookRepo = InMemoryBookRepositoryInterpreter[F]()
-      bookRepo       = DoobieBookRepository[F](xa)
-      bookValidation = BookValidation[F](bookRepo)
-      bookService    = BookService[F](bookRepo, bookValidation)
-      services       = BookEndpoints.endpoints[F](bookService)
-      httpApp        = Router("/" -> services).orNotFound
+      bookRepo         = DoobieBookRepository[F](xa)
+      cartRepo         = DoobieCartRepository[F](xa)
+      bookValidation   = BookValidation[F](bookRepo)
+      bookService      = BookService[F](bookRepo, bookValidation)
+      discountFromCart = DiscountFromCart[F](cartRepo)
+      services         = BookEndpoints.endpoints[F](bookService) <+> CartEndpoints.endpoints[F](discountFromCart)
+      httpApp          = Router("/" -> services).orNotFound
       _ <- Resource.liftF(DatabaseConfig.initializeDb(conf.db))
       server <- BlazeServerBuilder[F]
         .bindHttp(conf.server.port, conf.server.host)
